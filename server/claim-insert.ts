a5f6b0dfeaaab6345f9d5b43c2bc65cbe1e4182d
Cornelius Oliver <mintvaultuk@gmail.com>
2026-05-20 20:08:02 +0100

    fix: remove Est. 2026 from claim insert footer

--- server/claim-insert.ts
+++ server/claim-insert.ts
@@ -262,7 +262,7 @@ export async function generateClaimInsertPNG(
   ctx.fillStyle = "#000000";
   ctx.textAlign = "center";
   ctx.fillText(
-    "mintvaultuk.com  \u00b7  MintVault UK  \u00b7  Est. 2026",
+    "mintvaultuk.com  \u00b7  MintVault UK",
     PX_W / 2,
     footerTextTop,
   );
